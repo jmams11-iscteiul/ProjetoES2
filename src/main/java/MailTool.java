86270d6f0ec10d39c3b534d6df0542348e8cef42
trmfa <trmfa@iscte-iul.pt>
2020-06-14 23:16:28 +0100

    mail doesnt send if it doesnt have anything to say

--- src/main/java/MailTool.java
+++ src/main/java/MailTool.java
@@ -94,26 +94,28 @@ public class MailTool {
 	}
 
 	public static void sendEmail(String from, String to, String username, String password) {
-		Properties properties = new Properties();
-		properties.put("mail.smtp.host", "smtp.sapo.pt");
-		properties.put("mail.smtp.port", "587");
-		properties.put("mail.smtp.auth", "true");
-		properties.put("mail.smtp.starttls.enable", "true");
+		if (!errorField.equals("")) {
+			Properties properties = new Properties();
+			properties.put("mail.smtp.host", "smtp.sapo.pt");
+			properties.put("mail.smtp.port", "587");
+			properties.put("mail.smtp.auth", "true");
+			properties.put("mail.smtp.starttls.enable", "true");
 
-		Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(username, password);
+			Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(username, password);
+				}
+			});
+			try {
+				MimeMessage message = new MimeMessage(session);
+				message.setFrom(new InternetAddress(from));
+				message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+				message.setSubject("Errors in your website");
+				message.setText(errorField);
+				Transport.send(message);
+			} catch (MessagingException mex) {
+				mex.printStackTrace();
 			}
-		});
-		try {
-			MimeMessage message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(from));
-			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-			message.setSubject("Errors in your website");
-			message.setText(errorField);
-			Transport.send(message);
-		} catch (MessagingException mex) {
-			mex.printStackTrace();
 		}
 	}
 
